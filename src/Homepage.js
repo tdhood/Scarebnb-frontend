96a4daf39eb506f8df18a55ff9bb8569465b4ad3
tdhood <taylordannhood@gmail.com>
2022-07-29 10:27:33 -0700

    homepage!

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,6 +1,7 @@
-import { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import userContext from "./userContext";
 import React from "react";
+import ShareBnBApi from "./api";
 
 /**Shows homepage
  *
@@ -13,11 +14,29 @@ import React from "react";
 
 function Homepage() {
     console.log('homepage')
-    const [ listings, setListing ] = useState({
+    const [ listings, setListings ] = useState({
         data: null,
         isLoading: true,
     });
 
+    useEffect(function getListingsOnMount() {
+        console.log("getListingsOnMount");
+        async function fetchListings() {
+            const response = await ShareBnBApi.getListings();
+            setListings({data: response, isLoading: false})
+        }
+        fetchListings();
+    },
+    []
+    );
 
-
+    return (
+        <div className="Homepage">
+            {listings.data.map((listing) => (
+                <ListingCard key={listing.id} listing={listing}/>
+            ))}
+        </div>
+    )
 }
+
+export default Homepage;
